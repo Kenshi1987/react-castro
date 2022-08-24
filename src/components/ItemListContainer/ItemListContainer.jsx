f97c392c7de406e8389af326499070ce733141d6
Kenshi1987 <diegocabe87@gmail.com>
2022-08-24 13:41:34 -0300

    Cambios

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Spinner } from '@chakra-ui/react';
+import ItemList from '../ItemList/ItemList';
 
 export default function ItemListContainer({greeting}) {
+  const [items, setItems] = useState ({});
+
+  useEffect(()=> {
+
+    let products = [{id: 1, title: 'Aceites Esenciales', price: '$ 500'}, 
+                    {id: 2, title: 'Velas Aromaticas', price: '$500'}];
+      new Promise((resolve, reject) => {
+        
+        setTimeout(()=>{
+            resolve(products);
+
+        },2000)
+      }).then((data)=>{
+        setItems(data)
+      })
+  }, [])
+
   return (
     <div>
-        <span style={{border:'3px solid black',backgroundColor:'lightgrey', margin:'50px'}}>{greeting}</span>
+        <span>{greeting}</span>
+        {items.length ?
+          <ItemList items={items}/> :
+          <Spinner></Spinner>
+        }
     </div>
   )
 }
+
+export {ItemListContainer};
